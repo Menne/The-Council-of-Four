6c0c292dbfd5f6fd9ad60e316b535e43c4c53e68
Menne <emanuele1.torelli@mail.polimi.it>
2016-04-24 13:22:23 +0200

    Player and Score classes

--- src/main/java/players/Player.java
+++ src/main/java/players/Player.java
@@ -1,12 +1,64 @@
 package players;
 
+	/** Player class constains all of the informations abaut a player
+	 * 
+	 * @author Emanuele
+	 *
+	 */
+
 public class Player {
 
-	private int playerNumber;
+	private final int playerNumber;
+	private final String name;
 	private int assistants;
-	private int score;
+	private Score score;
 	private int nobility;
 	private int coins;
 	private boolean isPlayerTurn;
+	
+	public Player() {
+		this.playerNumber=playerNumber;
+		this.name=name;
+		this.assistants=assistants;
+		this.score=score;
+		this.nobility=nobility;
+		this.coins=coins;
+		this.isPlayerTurn=isPlayerTurn;
+	}
 
+	public getNumber() {
+		return number;
+	}
+	
+	public getName() {
+		return name;
+	}
+	
+	public getAssistants() {
+		return assistants;
+	}
+	
+	public getScore() {
+		return score;
+	}
+	
+	public getNobility() {
+		return nobility;
+	}
+	
+	public getCoins() {
+		return coins;
+	}
+	
+	public void incrementAssistants(int increment) {
+		this.assistsants+=increment;
+	}
+	
+	public void incrementNobility(int increment) {
+		this.nobility+=increment;
+	}
+	
+	public void incrementCoins(int increment) {
+		this.coins+=increment;
+	}
 }
---
+++ src/main/java/players/Score.java
@@ -0,0 +1,21 @@
+package players;
+
+	/** Player's score
+	 * 
+	 * @author Emanuele
+	 *
+	 */
+
+public class Score {
+	
+	private int score;
+	
+	public Score() {
+		this.score=score;
+	}
+	
+	public incrementScore(int increment) {
+		this.score=this.score+increment;
+	}
+	
+}
