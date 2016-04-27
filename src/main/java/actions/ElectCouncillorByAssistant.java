a129399e91f56ace846c1037f4ebb8dc0a67ebc4
Scanna <luca.scannapieco@mail.polimi.it>
2016-04-27 19:37:22 +0200

    javadoc delle due quick action

--- src/main/java/actions/ElectCouncillorByAssistant.java
+++ src/main/java/actions/ElectCouncillorByAssistant.java
@@ -4,6 +4,14 @@ import gameStuff.CouncilBalcony;
 import gameStuff.Councillor;
 import model.Game;
 
+/**
+ * It's the quick action "elect councillor" it operates on the 
+ * protected attribute game through the method executeAction.
+ * The controller will pass to the constructor the councillor to add,
+ * and the balcony where he wants to add it. 
+ * @author Luca
+ *
+ */
 public class ElectCouncillorByAssistant extends QuickAction {
 
 	private final Councillor newCouncillor;
@@ -14,7 +22,13 @@ public class ElectCouncillorByAssistant extends QuickAction {
 		this.newCouncillor=newCouncillor;
 		this.councilBalcony=councilBalcony;
 	}
-	
+
+	/**
+	  * Substitutes a given councillor in one of the balconies of the game,
+	  * adds the old substituted councillor in the reserve,
+	  * decrement an assistant to the current player.
+	  * @return TRUE if the action ends well; FALSE otherwise.
+	 */
 	public boolean executeAction() {
 		Councillor oldCouncillor;
 		if((!this.game.getGameTable().getCouncilReserve()
