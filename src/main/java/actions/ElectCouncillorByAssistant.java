fb3a5b866e910046fd4359764c9d8c568a34143f
Scanna <luca.scannapieco@mail.polimi.it>
2016-04-27 19:32:29 +0200

    Fatte 2 azioni veloci

--- src/main/java/actions/ElectCouncillorByAssistant.java
+++ src/main/java/actions/ElectCouncillorByAssistant.java
@@ -1,16 +1,30 @@
 package actions;
 
+import gameStuff.CouncilBalcony;
+import gameStuff.Councillor;
 import model.Game;
 
 public class ElectCouncillorByAssistant extends QuickAction {
 
-	/**
-	 * 
-	 * @param p
-	 */
+	private final Councillor newCouncillor;
+	private final CouncilBalcony councilBalcony;
+	
+	public ElectCouncillorByAssistant(Game game, Councillor newCouncillor, CouncilBalcony councilBalcony){
+		super(game);
+		this.newCouncillor=newCouncillor;
+		this.councilBalcony=councilBalcony;
+	}
+	
 	public boolean executeAction() {
-		// TODO - implement ElectCouncilloByAssistant.executeAction
-		throw new UnsupportedOperationException();
+		Councillor oldCouncillor;
+		if((!this.game.getGameTable().getCouncilReserve()
+				.getCouncillors().contains(newCouncillor))||
+				(!this.game.getCurrentPlayer().decrementAssistants(1)))
+			return false;
+		oldCouncillor=this.councilBalcony.substituteCouncillor(newCouncillor);
+		this.game.getGameTable().getCouncilReserve().getCouncillors().add(oldCouncillor);
+		this.game.getCurrentPlayer().decrementAssistants(1);
+		return true;
 	}
 
 }
