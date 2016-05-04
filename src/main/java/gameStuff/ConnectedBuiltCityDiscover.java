0c7308522e280e5a9c2636969f92fc0447c948d2
Scanna <luca.scannapieco@mail.polimi.it>
2016-05-04 12:56:31 +0200

    Aggiunta classe per il controllo dei bonus da ottenere dopo aver costruito

---
+++ src/main/java/gameStuff/ConnectedBuiltCityDiscover.java
@@ -0,0 +1,51 @@
+package gameStuff;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.jgraph.graph.DefaultEdge;
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.alg.ConnectivityInspector;
+
+public class ConnectedBuiltCityDiscover {
+
+	private final Set<City> connectedBonusCities;
+	private final Set<City> checkedCities;
+	
+	public ConnectedBuiltCityDiscover(){
+		this.connectedBonusCities=new HashSet<City>();
+		this.checkedCities=new HashSet<City>();
+	}
+	
+	private void addCities(UndirectedGraph<City,DefaultEdge> map, 
+			City cityToCheck, Emporium builedEmporium){
+		
+		this.checkedCities.add(cityToCheck);
+		boolean check=false;
+		for(int i=0; i<cityToCheck.getCitysEmporiums().length; i++)
+			if(cityToCheck.getCitysEmporiums()[i].equals(builedEmporium))
+				check=true;
+		if(!check)
+			return;
+		else{
+			this.connectedBonusCities.add(cityToCheck);
+			ConnectivityInspector<City,DefaultEdge> inspector=
+					new ConnectivityInspector<City,DefaultEdge>(map);
+			for(City nearCity : inspector.connectedSetOf(cityToCheck))
+				if(!this.checkedCities.contains(nearCity))
+					this.addCities(map, nearCity, builedEmporium);			
+		}			
+	}
+	
+	private void resetAttributes(){
+		this.checkedCities.clear();
+		this.connectedBonusCities.clear();
+	}
+	
+	public Set<City> getConnectedBuiltCities(UndirectedGraph<City,DefaultEdge> map, 
+			City cityToCheck, Emporium builedEmporium){
+		this.addCities(map, cityToCheck, builedEmporium);
+		Set<City> temp= new HashSet<City>(this.connectedBonusCities);
+		this.resetAttributes();
+		return temp;
+	}
+}
