0286b6262a2f3561902c57f490b744181da50712
Scanna <luca.scannapieco@mail.polimi.it>
2016-05-04 18:29:37 +0200

    fatta classe nobility track

--- src/main/java/gameStuff/NobilityTrack.java
+++ src/main/java/gameStuff/NobilityTrack.java
@@ -1,19 +1,45 @@
 package gameStuff;
 
-import java.util.Map;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import bonus.Bonus;
+
+/**
+ * It's a List of sets of bonuses.
+ * @author Media
+ *
+ */
 public class NobilityTrack {
 
-	private final Map<Integer,Bonus> track;
+	private final List<Set<Bonus>> track;
 	
-	public NobilityTrack(Map<Integer, Bonus> track){
-		this.track=track;
+	/**
+	 * Initialize the track with empty sets of bonuses.
+	 * The length of the track is given in input.
+	 * @param lenght of the track.
+	 */
+	public NobilityTrack(int lenght){
+		this.track=new ArrayList<Set<Bonus>>();
+		for(int i=0; i<lenght; i++)
+			this.track.add(new HashSet<Bonus>());
 	}
 
-	public Map<Integer, Bonus> getTrack() {
+	public List<Set<Bonus>> getTrack() {
 		return track;
 	}
+	
+	/**
+	 * Adds a given set of bonuses at the specified index
+	 * @param index where to put the set of bonuses
+	 * @param bonuses to add
+	 */
+	public void setBonus(int index, Set<Bonus> bonuses){
+		this.track.add(index, bonuses);
+	}
+	
+	
 	
 	
 
