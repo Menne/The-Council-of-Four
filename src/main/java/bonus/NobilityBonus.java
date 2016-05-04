d29cf3ec95dbd62d303aadc35318091c2febaa70
Scanna <luca.scannapieco@mail.polimi.it>
2016-05-04 18:53:49 +0200

    aggiunta nobility track in game table e aggiornato il bonus nobility

--- src/main/java/bonus/NobilityBonus.java
+++ src/main/java/bonus/NobilityBonus.java
@@ -21,11 +21,16 @@ public class NobilityBonus implements Bonus{
 	}
 	
 	/**
-	 * Assigns to the currentPlayer an advancement in the nobility track
+	 * Assigns to the currentPlayer an advancement in the nobility track.
+	 * If the new nobility slot has bonuses inside it will assign them to the player
 	 * @param currentPlayer is the player who is playing the turn
 	 */
 	public void assignBonus(Game game) {
 		game.getCurrentPlayer().incrementNobility(nobilityAdvancement);
+		int currentNobility=game.getCurrentPlayer().getNobility();
+		if(!game.getGameTable().getNobilityTrack().getTrack().get(currentNobility).isEmpty())
+			for(Bonus bonus : game.getGameTable().getNobilityTrack().getTrack().get(currentNobility))
+				bonus.assignBonus(game);			
 	}
 
 }
