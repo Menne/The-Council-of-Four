91f9f380e5a00eda3c410db1d774ec956e7e7974
Scanna <luca.scannapieco@mail.polimi.it>
2016-05-05 18:09:22 +0200

    completata classe councillors reserve

--- src/main/java/gameStuff/CouncillorsReserve.java
+++ src/main/java/gameStuff/CouncillorsReserve.java
@@ -1,7 +1,11 @@
 package gameStuff;
 
 import java.util.List;
-
+/**
+ * Models the councillor reserve of game
+ * @author Luca
+ *
+ */
 public class CouncillorsReserve {
 	
 	private final List<Councillor> councillors;
@@ -9,25 +13,29 @@ public class CouncillorsReserve {
 	public CouncillorsReserve(List<Councillor> councillors){
 		this.councillors=councillors;
 	}
-
-	/**
-	 * 
-	 * @param c
-	 */
-	public void addConcullor(Councillor c) {
-
-	}
-
+	
 	public List<Councillor> getCouncillors() {
 		return councillors;
 	}
+	
+	/**
+	 * Adds a given councillor in the reserve
+	 * @param c is councillor to add
+	 */
+	public void addConcullor(Councillor c) {
+		this.councillors.add(c);
+	}
+
+
 
 	/**
-	 * 
-	 * @param c
+	 * Remove a given councillor from the reserve
+	 * @param c is the councillor to remove
 	 */
-	public void removeCouncillor(Councillor c) {
-
+	public void removeCouncillor(Councillor c) throws IndexOutOfBoundsException{
+		if(!this.councillors.isEmpty())
+			this.councillors.remove(c);
+		throw new IndexOutOfBoundsException("No councillors in the reserve");
 	}
 
 	@Override
