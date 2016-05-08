981e7610fa90e4fcaaaf18593d8a4161bdfcaebe
Menne <emanuele1.torelli@mail.polimi.it>
2016-05-08 11:37:43 +0200

    corretti errori di compilazione

--- src/main/java/controller/ActionExecutor.java
+++ src/main/java/controller/ActionExecutor.java
@@ -2,7 +2,16 @@ package controller;
 
 import java.util.List;
 
+import actions.AcquirePermitTile;
 import actions.Action;
+import actions.BuildByPermitTile;
+import actions.ElectCouncillor;
+import gameStuff.City;
+import gameStuff.CouncilBalcony;
+import gameStuff.Councillor;
+import gameStuff.PermitTile;
+import gameStuff.PoliticsCard;
+import gameStuff.RegionBoard;
 import model.Game;
 
 public class ActionExecutor {
@@ -18,77 +27,77 @@ public class ActionExecutor {
 	}
 	
 	public void callAction(){
-//		Action action=this.actionTransaltor();
-//		action.executeAction();
+		Action action=this.actionTranslator();
+		action.executeAction();
 	}
 	
 	/**
 	 * TODO riceve azione e parametro sottoforma di stringa,
-	 *  crea e ritorna l'azione corrispondente
+	 * crea e ritorna l'azione corrispondente
 	 * @return
-<<<<<<< HEAD
 	 */
 	private Action actionTranslator(){
 		if(action.equals("acquire permit tile")) {
-			Action acquirePermitTile=new AcquirePermitTile(this.game) ;
-				acquirePermitTile.set(this.parameters.remove(0)), 
-				acquirePermitTile.set(this.parameters.remove(0)), 
-				acquirePermitTile.(this.parameters);
+			AcquirePermitTile acquirePermitTile=new AcquirePermitTile(this.game);
+			acquirePermitTile.setChosenRegion(regionTranslator
+					(this.parameters.remove(0)));
+			acquirePermitTile.setNumberOfPermitTile(numberOfPermitTileTranslator
+					(this.parameters.remove(0))); 
+			acquirePermitTile.setCardsToDescard(politicsCardsTranslator
+					(this.parameters));
 			return acquirePermitTile;
 		}
 		if(action.equals("build by permit tile")) {
-			Action buildByPermitTile=new BuildByPermitTile(this.game, 
-					this.PermitTileTranslator(this.parameters.remove(0)), 
-					this.cityTranslator(this.parameters.remove(0)));
+			BuildByPermitTile buildByPermitTile=new BuildByPermitTile(this.game);
+			buildByPermitTile.setSelectedCity(cityTranslator
+					(this.parameters.remove(0)));
+			buildByPermitTile.setSelectedPermitTile(permitTileTranslator
+					(this.parameters.remove(0)));
 			return buildByPermitTile;
 		}
-		if(action.equals("M3")) {
-			Action acquirePermitTile=new AcquirePermitTile(this.game, 
-					this.regionTranslator(this.parameters.remove(0)), 
-					this.numberOfPermitTileTranslator(this.parameters.remove(0)), 
-					this.politicsCardsTranslator(this.parameters));
-			return acquirePermitTile;
+		if(action.equals("ecelct councillor")) {
+			ElectCouncillor electCouncillor=new ElectCouncillor(this.game);
+			electCouncillor.setNewCouncillor(councillorTranslator
+					(this.parameters.remove(0)));
+			electCouncillor.setCouncilBalcony(councilBalconyTranslator
+					(this.parameters.remove(0)));
+			return electCouncillor;
 		}
 		if(action.equals("M4")) {
-			Action acquirePermitTile=new AcquirePermitTile(this.game, 
-					this.regionTranslator(this.parameters.remove(0)), 
-					this.numberOfPermitTileTranslator(this.parameters.remove(0)), 
-					this.politicsCardsTranslator(this.parameters));
+			Action acquirePermitTile=new AcquirePermitTile(this.game);
 			return acquirePermitTile;
 		}
 		if(action.equals("q1")) {
-			Action acquirePermitTile=new AcquirePermitTile(this.game, 
-					this.regionTranslator(this.parameters.remove(0)), 
-					this.numberOfPermitTileTranslator(this.parameters.remove(0)), 
-					this.politicsCardsTranslator(this.parameters));
+			Action acquirePermitTile=new AcquirePermitTile(this.game);
 			return acquirePermitTile;
 		}
 		if(action.equals("2q")) {
-			Action acquirePermitTile=new AcquirePermitTile(this.game, 
-					this.regionTranslator(this.parameters.remove(0)), 
-					this.numberOfPermitTileTranslator(this.parameters.remove(0)), 
-					this.politicsCardsTranslator(this.parameters));
+			Action acquirePermitTile=new AcquirePermitTile(this.game);
 			return acquirePermitTile;
 		}
 		if(action.equals("3q")) {
-			Action acquirePermitTile=new AcquirePermitTile(this.game, 
-					this.regionTranslator(this.parameters.remove(0)), 
-					this.numberOfPermitTileTranslator(this.parameters.remove(0)), 
-					this.politicsCardsTranslator(this.parameters));
+			Action acquirePermitTile=new AcquirePermitTile(this.game);
 			return acquirePermitTile;
 		}
 		if(action.equals("4q")) {
-			Action acquirePermitTile=new AcquirePermitTile(this.game, 
-					this.regionTranslator(this.parameters.remove(0)), 
-					this.numberOfPermitTileTranslator(this.parameters.remove(0)), 
-					this.politicsCardsTranslator(this.parameters));
+			Action acquirePermitTile=new AcquirePermitTile(this.game);
 			return acquirePermitTile;
 		}
 		return null;
 		
 	}
 
-	private PermitTile PermitTileTranslator(String permitTileToTranslate) {
+	private CouncilBalcony councilBalconyTranslator(String councilBalconyToTranslate) {
+		CouncilBalcony councilBalconyTranslator = null;
+		return councilBalconyTranslator;
+	}
+
+	private Councillor councillorTranslator(String newCouncillorToTranslate) {
+		Councillor newCouncillorTranslated = null;
+		return newCouncillorTranslated;
+	}
+
+	private PermitTile permitTileTranslator(String permitTileToTranslate) {
 		PermitTile permitTileTranslator = null;
 		return permitTileTranslator;
 	}
@@ -113,12 +122,6 @@ public class ActionExecutor {
 	private RegionBoard regionTranslator(String regionToTranslate) {
 		RegionBoard regionTranslated=null;
 		return regionTranslated;
-=======
-	 
-	private Action actionTransaltor(){
-		if(action.equals("M1"))
->>>>>>> branch 'master' of https://Menne@bitbucket.org/Scanna/the-council-of-four.git
 	}
 	
-	**/
 }
