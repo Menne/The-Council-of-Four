8b9c4844e34b3580b8f3991d7335a889bbeec0c2
Scanna <luca.scannapieco@mail.polimi.it>
2016-05-24 17:02:25 +0200

    gestiti bonus nel modelDTO

--- src/main/java/client/ModelDTO/GameDTO.java
+++ src/main/java/client/ModelDTO/GameDTO.java
@@ -3,10 +3,11 @@ package client.ModelDTO;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import client.actionDTO.ActionDTO;
 import model.Game;
-import model.gameTable.CardColour;
+import model.bonus.Bonus;
 import model.gameTable.CouncilBalcony;
 import model.gameTable.Councillor;
 import model.gameTable.RegionBoard;
@@ -17,7 +18,7 @@ public class GameDTO implements ModelDTO<Game>{
 	private List<RegionDTO> clientRegions;
 	private CardColourDTO[] clientKingBalcony;
 	private List<CardColourDTO> clientCouncillorReserve;
-//	private List<Set<String>> clientNobilityTrack;
+	private List<Set<Bonus>> clientNobilityTrack;
 	private List<PlayerDTO> clientPlayers;
 	private PlayerDTO currentPlayer;
 	private List<ActionDTO> availableActions;
@@ -26,7 +27,7 @@ public class GameDTO implements ModelDTO<Game>{
 		this.clientRegions=new ArrayList<RegionDTO>();
 		this.clientKingBalcony=new CardColourDTO[4];
 		this.clientCouncillorReserve=new ArrayList<CardColourDTO>();
-//		this.clientNobilityTrack=new ArrayList<Set<String>>();
+		this.clientNobilityTrack=new ArrayList<Set<Bonus>>();
 		this.clientPlayers=new ArrayList<PlayerDTO>();
 	}
 
@@ -55,9 +56,67 @@ public class GameDTO implements ModelDTO<Game>{
 			playerDTO.map(player);
 			this.clientPlayers.add(playerDTO);
 		}
+		
+		this.clientNobilityTrack=realObject.getGameTable().getNobilityTrack().getTrack();
 	}
 
 	
+	public List<RegionDTO> getClientRegions() {
+		return clientRegions;
+	}
+
+	public void setClientRegions(List<RegionDTO> clientRegions) {
+		this.clientRegions = clientRegions;
+	}
+
+	public CardColourDTO[] getClientKingBalcony() {
+		return clientKingBalcony;
+	}
+
+	public void setClientKingBalcony(CardColourDTO[] clientKingBalcony) {
+		this.clientKingBalcony = clientKingBalcony;
+	}
+
+	public List<CardColourDTO> getClientCouncillorReserve() {
+		return clientCouncillorReserve;
+	}
+
+	public void setClientCouncillorReserve(List<CardColourDTO> clientCouncillorReserve) {
+		this.clientCouncillorReserve = clientCouncillorReserve;
+	}
+
+	public List<Set<Bonus>> getClientNobilityTrack() {
+		return clientNobilityTrack;
+	}
+
+	public void setClientNobilityTrack(List<Set<Bonus>> clientNobilityTrack) {
+		this.clientNobilityTrack = clientNobilityTrack;
+	}
+
+	public List<PlayerDTO> getClientPlayers() {
+		return clientPlayers;
+	}
+
+	public void setClientPlayers(List<PlayerDTO> clientPlayers) {
+		this.clientPlayers = clientPlayers;
+	}
+
+	public PlayerDTO getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(PlayerDTO currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+
+	public List<ActionDTO> getAvailableActions() {
+		return availableActions;
+	}
+
+	public void setAvailableActions(List<ActionDTO> availableActions) {
+		this.availableActions = availableActions;
+	}
+
 	@Override
 	public String toString() {
 		return "GameDTO [clientRegions=" + clientRegions + ", clientKingBalcony="
--- src/main/java/client/ModelDTO/PermitTileDTO.java
+++ src/main/java/client/ModelDTO/PermitTileDTO.java
@@ -1,5 +1,52 @@
 package client.ModelDTO;
 
-public class PermitTileDTO {
+import java.util.HashSet;
+import java.util.Set;
 
+import model.bonus.Bonus;
+import model.gameTable.City;
+import model.gameTable.PermitTile;
+
+public class PermitTileDTO implements ModelDTO<PermitTile>{
+
+	private Set<CityDTO> buildablecities;
+	private Set<Bonus> bonuses;
+	
+	public PermitTileDTO(){
+		this.buildablecities=new HashSet<CityDTO>();
+		this.bonuses=new HashSet<Bonus>();
+	}
+
+	@Override
+	public void map(PermitTile realObject) {
+		for(City city : realObject.getBuildableCities()){
+			CityDTO cityDTO=new CityDTO();
+			cityDTO.map(city);
+			this.buildablecities.add(cityDTO);
+		}
+		this.bonuses=realObject.getBonus();		
+	}
+
+	public Set<CityDTO> getBuildablecities() {
+		return buildablecities;
+	}
+
+	public void setBuildablecities(Set<CityDTO> buildablecities) {
+		this.buildablecities = buildablecities;
+	}
+
+	public Set<Bonus> getBonuses() {
+		return bonuses;
+	}
+
+	public void setBonuses(Set<Bonus> bonuses) {
+		this.bonuses = bonuses;
+	}
+
+	@Override
+	public String toString() {
+		return "PermitTileDTO [buildablecities=" + buildablecities + ", bonuses=" + bonuses + "]";
+	}
+	
+	
 }
