a642fc0ce332c3fb2fd0afc4d7d00d7140b85f70
squalipa <andrea1.pasquali@mail.polimi.it>
2016-06-02 11:42:32 +0200

    test market

---
+++ src/test/java/marketTests/MarketTest.java
@@ -0,0 +1,72 @@
+package marketTests;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import players.Player;
+import server.model.Game;
+import server.model.gameTable.Assistant;
+import server.model.market.Market;
+import server.model.market.Offer;
+import server.model.stateMachine.SellingState;
+
+public class MarketTest {
+
+	@Test
+	public void test() throws IOException {
+		Game game=new Game();
+		List<Player> players = new ArrayList<>();
+		Player a = new Player();
+		Player b= new Player();
+		players.add(a);
+		players.add(b);
+		game.start(players);
+		game.setCurrentPlayer(a);
+		game.setState(new SellingState());
+		List<Player> sellingPlayers= new ArrayList<>();
+		List<Player> buyingPlayers= new ArrayList<>();
+		sellingPlayers.add(a);
+		buyingPlayers.add(b);
+		Market market= new Market(players);
+		market.sellingNextPlayer();
+		assertTrue(players==market.getSellingPlayerList());
+		assertTrue(market.getBuyingPlayerList().contains(players.get(0)));
+		assertTrue(a==market.getCurrentPlayer());
+		Assistant assistant= new Assistant();
+		assistant.addObjectToPlayer(a);
+		Offer offer= new Offer(a, assistant, 5);
+		market.addOffer(a, assistant, 5);
+		assertEquals(offer, market.getOffersList().get(0));
+		int oldCoinsBuyer= b.getCoins();
+		int oldCoinsSeller=a.getCoins();
+		int OldNumberOfAssistantsBuyer= b.getNumberOfAssistants();
+		int OldNumberOfAssistantsSeller=a.getNumberOfAssistants();
+		market.buyOffer(market.getOffersList().get(0), b);
+		assertEquals(oldCoinsBuyer-5, b.getCoins());
+		assertEquals(oldCoinsSeller+5, a.getCoins());
+		assertEquals(OldNumberOfAssistantsBuyer+1, b.getNumberOfAssistants());
+		assertEquals(OldNumberOfAssistantsSeller-1, a.getNumberOfAssistants());
+		assertTrue(market.getOffersList().isEmpty());
+		Player c= new Player();
+		market.addPlayer(c);
+		assertTrue(market.getBuyingPlayerList().contains(c));
+		assertFalse(market.isSellingPhaseFinished());
+		market.sellingNextPlayer();
+		assertTrue(market.isSellingPhaseFinished());
+		market.buyingNextPlayer();
+		assertTrue(market.getCurrentPlayer()==a);
+		market.buyingNextPlayer();
+		assertFalse(market.isBuyingPhaseFinished());
+		market.buyingNextPlayer();
+		assertTrue(market.isBuyingPhaseFinished());
+		market.addOffer(a, assistant, 2);
+		market.clearMarket();
+		assertTrue(market.getOffersList().isEmpty());
+	}
+
+}
---
+++ src/test/java/marketTests/OfferTest.java
@@ -0,0 +1,23 @@
+package marketTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import players.Player;
+import server.model.gameTable.Assistant;
+import server.model.market.Offer;
+
+public class OfferTest {
+
+	@Test
+	public void test() {
+		Player a=new Player();
+		Assistant assistant=new Assistant();
+		Offer offer=new Offer(a, assistant, 5);
+		assertTrue(a== offer.getOfferingPlayer());
+		assertTrue(assistant== offer.getOfferedObject());
+		assertEquals(5, offer.getPrice());
+	}
+
+}
