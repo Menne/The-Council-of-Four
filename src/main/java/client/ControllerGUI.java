8d3cb2ed7b196070ab369b06ff9cac517b34743f
Scanna <luca.scannapieco@mail.polimi.it>
2016-06-20 18:41:26 +0200

    stampati players

--- src/main/java/client/ControllerGUI.java
+++ src/main/java/client/ControllerGUI.java
@@ -442,7 +442,6 @@ public class ControllerGUI {
 	@FXML
 	private TextArea messageBox;
 
-	
 	public HBox getHand() {
 		return hand;
 	}
@@ -494,6 +493,14 @@ public class ControllerGUI {
 
 	public VBox getCouncillorReserve(){
 		return councillorReserve;
+	}
+	
+	public List<Label> getNumberOfPoliticsCards(){
+		return Arrays.asList(numberOfPoliticsCardInHandPlayer1,numberOfPoliticsCardInHandPlayer2,numberOfPoliticsCardInHandPlayer3,numberOfPoliticsCardInHandPlayer4);
+	}
+	
+	public List<Label> getNumberOfCoveredPermitTiles(){
+		return Arrays.asList(numberOfCoveredPermitTilePlayer1,numberOfCoveredPermitTilePlayer2,numberOfCoveredPermitTilePlayer3,numberOfCoveredPermitTilePlayer4);
 	}
 	
 	public ImageView[] getSeaPermitTile(){
