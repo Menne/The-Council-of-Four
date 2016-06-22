25bc92628c92c734f03d2a0c07002df5f9975fb9
Menne <emanuele1.torelli@mail.polimi.it>
2016-06-22 17:15:51 +0200

    risolto errore

--- src/main/java/client/view/GUI.java
+++ src/main/java/client/view/GUI.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 
 import client.ControllerGUI;
 import client.connections.Connection;
