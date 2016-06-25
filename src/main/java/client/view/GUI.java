68c3cf8e4e70eac3bf5d0c3aeb448959c6adddd2
Scanna <luca.scannapieco@mail.polimi.it>
2016-06-25 19:04:36 +0200

    market

--- src/main/java/client/view/GUI.java
+++ src/main/java/client/view/GUI.java
@@ -1063,9 +1063,6 @@ public class GUI extends ClientView{
 		this.controllerGUI.getPoliticsDeck().setDisable(disabled);
 		for (Button button : this.controllerGUI.getActions())
 			button.setDisable(disabled);
-		this.controllerMarketGUI.getMakeAnOffer().setDisable(disabled);
-		this.controllerMarketGUI.getAcceptAnOffer().setDisable(disabled);
-		this.controllerMarketGUI.getSkip().setDisable(disabled);
 	}
 	
 
