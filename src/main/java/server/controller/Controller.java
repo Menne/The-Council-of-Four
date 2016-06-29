ec4303ceed3ea0d468fed74b53adb1bfaecc87c7
Scanna <luca.scannapieco@mail.polimi.it>
2016-06-29 18:15:55 +0200

    javadoc states

--- src/main/java/server/controller/Controller.java
+++ src/main/java/server/controller/Controller.java
@@ -4,21 +4,33 @@ import observerPattern.Observer;
 import server.model.Game;
 import server.model.actions.Action;
 
+/**
+ * Controller of the MVC on the server side
+ * @author Luca Scannapieco
+ *
+ */
 public class Controller implements Observer<Action>{
 	
 	private final Game game;
 	
+	/**
+	 * @param game The game on which the controller will execute the actions
+	 */
 	public Controller(Game game){
 		this.game=game;
 	}
 	
 
-
+	/**
+	 * @return the game on which the controller executes the actions.
+	 */
 	public Game getGame() {
 		return game;
 	}
 
-	
+	/**
+	 * This method receives actions from the Server View and execute them.
+	 */
 	@Override
 	public void update(Action action){
 		
