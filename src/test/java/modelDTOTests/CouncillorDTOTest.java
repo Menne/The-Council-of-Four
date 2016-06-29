d1e73dabc1547cf4522f7ab397c428ebd4dcd3ed
Andrea Pasquali <andrea1.pasquali@mail.polimi.it>
2016-06-29 17:24:17 +0200

    Initializer rinnovato e cursore gioco customizzabile

---
+++ src/test/java/modelDTOTests/CouncillorDTOTest.java
@@ -0,0 +1,27 @@
+package modelDTOTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import client.modelDTO.gameTableDTO.CardColourDTO;
+import client.modelDTO.gameTableDTO.CouncillorDTO;
+
+public class CouncillorDTOTest {
+
+	@Test
+	public void test() {
+		CouncillorDTO councillor= new CouncillorDTO();
+		CouncillorDTO councillor1= new CouncillorDTO();
+		CardColourDTO colour= new CardColourDTO();
+		String a="blue";
+		colour.setName(a);
+		councillor.setColour(colour);
+		councillor1.setColour(colour);
+		assertTrue(councillor.getColour()==colour);
+		assertTrue(councillor.equals(councillor1));
+		assertTrue(councillor.toString()==a);
+		assertEquals(3027096, councillor.hashCode());
+	}
+
+}
