becbab384f71b888fe01e4cacc4d8bb63a83c1cf
Menne <emanuele1.torelli@mail.polimi.it>
2016-07-02 11:28:23 +0200

    javadoc + fix

--- src/main/java/server/model/actions/QuickAction.java
+++ src/main/java/server/model/actions/QuickAction.java
@@ -1,10 +1,6 @@
 package server.model.actions;
 
-import java.util.List;
-
 import server.model.Game;
-import server.model.player.Player;
-import server.view.notifies.ErrorNotify;
 
 /**
  * The class that models the generic quick actions of the game
@@ -12,10 +8,6 @@ import server.view.notifies.ErrorNotify;
  *
  */
 public interface QuickAction extends Action {
-	
-	public default void sendErrorNotify(Game game, List<Player> interestedPlayers){
-		game.notifyObserver(new ErrorNotify("You can't do this action", interestedPlayers));
-	}
 	
 	public default void nextState(Game game){
 		game.setState(game.getState().quickActionTransition(game));
