1665a7f9b2d7b238be63df9ec37c512137f4d8ea
Andrea Pasquali <andrea1.pasquali@mail.polimi.it>
2016-07-02 11:15:49 +0200

    test model DTO conclusi

--- src/test/java/modelDTOTests/CityColourDTOtest.java
+++ src/test/java/modelDTOTests/CityColourDTOtest.java
@@ -11,10 +11,14 @@ public class CityColourDTOtest {
 	@Test
 	public void test() {
 		CityColourDTO colour= new CityColourDTO();
+		CityColourDTO colour1= new CityColourDTO();
 		String name= "blu";
 		colour.setName(name);
+		colour1.setName(name);
 		assertTrue(colour.getName()==name);
 		assertEquals(name, colour.toString());
+		assertEquals(97674, colour.hashCode());
+		assertEquals(colour, colour1);
 	}
 	
 }
